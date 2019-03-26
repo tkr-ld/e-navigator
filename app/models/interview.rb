1c830a040d3965cf63d8c5ee13c8f028a2fea2e7
tkr_ld <tkr.ld.not.nyd@gmail.com>
2019-03-26 14:31:59 +0900

    面接承認メソッドのリファクタリング

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -3,36 +3,22 @@ class Interview < ApplicationRecord
 
   validates :start_datetime, presence: true
   validate :add_error_start_datetime
-  #validate :add_error_start_datetime, on: :update
 
   enum status: { reservation: 0, rejection: 1, approval: 2 }
 
   def add_error_start_datetime
     if start_datetime.past?
-      set_past_error
+      errors.add(:start_datetime, "は未来の時間を指定してください")
     end
   end
 
   def approve_datetime
-    if self.start_datetime.future?
-      self.approval!
-      #self.update!(status: :approval)
-
+    if self.update(status: :approval)
       user = self.user
       interviews = user.interviews.where.not(id: self.id)
-      interviews.each do |interview|
-        #interview.rejection! 
+      interviews.each do |interview| 
         interview.update_attribute(:status, :rejection)
       end
-    else
-      set_past_error
-      return false
     end
-  end
-
-  private
-
-  def set_past_error
-    errors.add(:start_datetime, "は未来の時間を指定してください")
   end
 end

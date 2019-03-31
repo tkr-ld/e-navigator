35dc1db93de0b04902939b9ca665ff0ff223227a
tkr_ld <tkr.ld.not.nyd@gmail.com>
2019-03-31 15:55:46 +0900

    本番環境のURL作成用のホスト名の追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'e-navigator-tkr-ld.herokuapp.com' }
 end
